1f755bb054c2f7f6778e1b8204499c5cac8bcd1f
Nathan Jent <n.jent@edaptive.com>
2016-09-02 17:19:51 -0400

    Initial idea for graph implementation.

--- src/day09.rs
+++ src/day09.rs
@@ -1,15 +1,33 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::collection::{HashMap, HashSet};
 
 pub fn single_night(file: &str) -> i32 {
     let input = File::open(file).expect("File open fail.");
     let reader = BufReader::new(input);
 
+    //TODO probably need a graph
+    let locs = HashSet::new();
+    let dists = HashMap::new();
     for line in reader.lines() {
         let line = line.unwrap();
         let s = String::from(line.trim());
         println!("{}", s);
-        //TODO probably need a graph
+
+        while let Some(w) = s.split_whitespace() {
+            if w != "to" && w != "=" {
+                let loc_dist = w.parse::<i32>();
+                match loc_dist {
+                    Ok(n) => {
+                        dists.insert(from_to, n);
+                    },
+                    Err(w) => {
+                        locs.insert(w);
+                    },
+                }
+            }
+        }
     }
+    -1
 }
