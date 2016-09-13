dc2594da9cf8c717dad63f1b26a329074eac2af8
nathanjent <nathanjent@gmail.com>
2016-09-12 23:27:23 -0400

    Update calculates one route. Need to search others.

--- src/day09.rs
+++ src/day09.rs
@@ -7,26 +7,15 @@ pub fn single_night(file: &str) -> i64 {
     let input = File::open(file).expect("File open fail.");
     let reader = BufReader::new(input);
 
-    #[derive(Debug)]
+    #[derive(Debug, Eq, Ord, PartialOrd)]
     struct Node(String, i64);
 
     impl PartialEq for Node {
         fn eq(&self, other: &Node) -> bool {
             self.0.eq(&other.0)
         }
-    }
-
-    impl Eq for Node {}
-
-    impl PartialOrd for Node {
-        fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
-    impl Ord for Node {
-        fn cmp(&self, other: &Node) -> Ordering {
-            self.1.cmp(&other.1)
+        fn ne(&self, other: &Node) -> bool {
+            !self.0.eq(&other.0)
         }
     }
 
@@ -46,8 +35,8 @@ pub fn single_night(file: &str) -> i64 {
         let from = Node(String::from(words[0]), i64::max_value());
         let to = Node(String::from(words[2]), i64::max_value());
         let distance = words[4]
-                           .parse::<i64>()
-                           .expect("parse error");
+            .parse::<i64>()
+            .expect("parse error");
 
         nodes.push(from.clone());
         nodes.push(to.clone());
@@ -68,31 +57,42 @@ pub fn single_night(file: &str) -> i64 {
     }
 
     println!("calculating...");
+    let mut out: Vec<Node> = Vec::new();
     loop {
         if nodes.is_empty() {
-            break;
+            let mut sum = 0;
+            for node in out {
+                sum += node.1;
+                println!("{:?}", node);
+            }
+            return sum;
         }
-        nodes.sort_by(|a, b| a.0.cmp(&b.0));
+        println!("sorted nodes");
+        nodes.sort_by(|a, b| b.1.cmp(&a.1));
+        for node in nodes.clone() {
+            println!("{:?}", node);
+        }
+
 
         let node = nodes.pop().unwrap();
-        println!("Got min {:?}", node);
+        println!("pop min {:?}", node);
 
         // for each neighbor of n
         for edge in edges.iter() {
             let (ref from, ref to, ref dist) = *edge;
             if from == &node {
+                println!("edge {:?} {:?} {:?}", from.0, to.0, dist);
                 let alt = node.1 + *dist;
                 if let Ok(neighbor_index) = nodes.binary_search(to) {
                     let mut neighbor = nodes.get_mut(neighbor_index).unwrap();
-                    // TODO assign new weight to neighbor
+                    // assign new weight to neighbor
                     if alt < neighbor.1 {
+                        println!("New weight {:?} {:?}", neighbor, alt);
                         *neighbor = Node(to.0.clone(), alt);
                     }
-                    println!("New weight {:?} {:?}", neighbor, alt);
                 }
-                // println!("{:?} {:?} {:?} {:?}", from, to, edge, node);
             }
         }
+        out.push(node);
     }
-    -1
 }
