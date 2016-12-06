6e38afe115a3fe6d550238a8be5ae3308bad62ee
nathanjent <nathanjent@gmail.com>
2016-12-05 22:40:29 -0500

    Progress splitting up into chars.

--- src/day04_2016.rs
+++ src/day04_2016.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::collections::BTreeMap;
 
 pub fn obsecurity_1(file: &str) -> usize {
     parse_rooms(file)
@@ -18,15 +19,29 @@ fn parse_rooms(file: &str) -> usize {
         .filter_map(Result::ok)
         .collect();
 
+    let mut sum = 0;
     for line in lines {
-        let words = line.split("-").collect::<Vec<&str>>();
-        println!("{:?}", words);
+        let mut words = line.split("-").collect::<Vec<&str>>();
+
+        if let Some(id_checksum) = words.pop() {
+            if let Some(i) = id_checksum.find("[") {
+                let (id, checksum) = id_checksum.split_at(i);
+                let checksum = checksum.trim_matches(|c| c == '[' || c == ']')
+                    .split("")
+                    .flat_map(str::chars)
+                    .collect::<Vec<char>>();
+                println!("{} {:?}", id, checksum);
+                let letters = words.iter().flat_map(|s| s.chars()).collect::<Vec<char>>();
+                println!("{:?}", letters);
+                let mut count = BTreeMap::new();
+                for c in letters {
+                    *count.entry(c).or_insert(0) += 1;
+                }
+                if let Ok(id) = id.parse::<usize>() {
+                    sum += id;
+                }
+            }
+        };
     }
     9
-}
-
-struct Room {
-    words: Vec<String>,
-    id: usize,
-    checksum: String,
 }
