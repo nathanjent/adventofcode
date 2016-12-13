24b260940f7681ca63abc8a074c12d241fb7f621
Nathan Jent <n.jent@edaptive.com>
2016-12-13 16:07:11 -0500

    Update day 8 2016.
    Operations are parsing.

--- src/day08_2016.rs
+++ src/day08_2016.rs
@@ -6,7 +6,7 @@ pub fn two_factor_1(file: &str) -> usize {
     count_pixels(file)
 }
 
-pub fn two_factor_1(file: &str) -> usize {
+pub fn two_factor_2(file: &str) -> usize {
     count_pixels(file)
 }
 
@@ -19,7 +19,50 @@ fn count_pixels(file: &str) -> usize {
         .collect();
 
     let mut count = 0;
-    for line in lines {
-    }
+    let operations = lines.iter().filter_map(|line| {
+        let terms = line.split_whitespace().collect::<Vec<&str>>();
+        match terms[0] {
+            "rect" => {
+                if let Some(i) = terms[1].find('x') {
+                    let (a, b) = terms[1].split_at(i);
+                    let a = a.parse::<usize>().unwrap_or(0);
+                    let b = b.parse::<usize>().unwrap_or(0);
+                    Some(Operation::Rect(a, b))
+                } else {
+                    None
+                }
+            },
+            "rotate" => {
+                if let Some(i) = terms[2].find('=') {
+                    let axis = match terms[1] {
+                        "row" => Axis::Y,
+                        "column" => Axis::X,
+                        _ => Axis::X,
+                    };
+                    let (_, a) = terms[2].split_at(i);
+                    let a = a.parse::<usize>().unwrap_or(0);
+                    let b = terms[4].parse::<usize>().unwrap_or(0);
+                    Some(Operation::Rotate(axis, a, b))
+                } else {
+                    None
+                }
+            },
+            _ => None,
+        }
+    })
+    .collect::<Vec<Operation>>();
+    println!("{:?}", operations);
     42
+}
+
+#[derive(Debug)]
+enum Operation {
+    Rect(usize, usize),
+    Rotate(Axis, usize, usize),
+}
+
+#[derive(Debug)]
+enum Axis {
+    X,
+    Y,
 }
