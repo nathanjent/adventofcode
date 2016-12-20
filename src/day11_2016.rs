88132bed7baee082f2c27d048e64e0b899d8a7ee
Nathan Jent <n.jent@edaptive.com>
2016-12-20 16:17:42 -0500

    Trying out this object structure.

--- src/day11_2016.rs
+++ src/day11_2016.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::collections::HashMap;
 
 pub fn rtg_1(file: &str) -> usize {
     process(file)
@@ -18,55 +19,105 @@ fn process(file: &str) -> usize {
         .filter_map(Result::ok)
         .collect();
 
-    for token in lines.iter().map(|line| {
-        line.split_whitespace()
-            .filter_map(|w| {
+    let mut state = HashMap::new();
+    for tokens in lines.iter().map(|line| {
+        let mut words = line.split_whitespace().rev();
+        let mut tokens = Vec::new();
+        loop {
+            if let Some(w) = words.next() {
+                let w = w.trim_matches(|c: char| !c.is_alphabetic());
                 match w {
-                    "first" => Some(Token::Floor(1)),
-                    "second" => Some(Token::Floor(2)),
-                    "third" => Some(Token::Floor(3)),
-                    "fourth" => Some(Token::Floor(4)),
-                    "promethium" => Some(Token::PromethiumGenerator),
-                    "cobalt" => Some(Token::CobaltGenerator),
-                    "curium" => Some(Token::CuriumGenerator),
-                    "ruthenium" => Some(Token::RutheniumGenerator),
-                    "promethium-compatible" => Some(Token::PromethiumMicrochip),
-                    "cobalt-compatible" => Some(Token::CobaltMicrochip),
-                    "curium-compatible" => Some(Token::CuriumMicrochip),
-                    "ruthenium-compatible" => Some(Token::RutheniumMicrochip),
-                    _ => None,
+                    "floor" => {
+                        if let Some(f) = words.next() {
+                            match f {
+                                "first" => tokens.push(Entity::Floor(1)),
+                                "second" => tokens.push(Entity::Floor(2)),
+                                "third" => tokens.push(Entity::Floor(3)),
+                                "fourth" => tokens.push(Entity::Floor(4)),
+                                _ => {},
+                            }
+                        }
+                    },
+                    "microchip" => {
+                        if let Some(m) = words.next() {
+                            if let Some(i) = m.find('-') {
+                                let (compatibility, _) = m.split_at(i);
+                                tokens.push(Entity::Microchip(compatibility))
+                            }
+                        }
+                    },
+                    "generator" => {
+                        if let Some(g) = words.next() {
+                            tokens.push(Entity::Generator(g))
+                        }
+                    },
+                    _ => {},
                 }
-            })
-        .collect::<Vec<Token>>()
-    })
-    .flat_map(|v| {
-        v.into_iter()
+            } else {
+                break;
+            }
+        }
+        tokens
     })
     {
-        match token {
-            Token::Floor(n) => {},
-            Token::PromethiumGenerator => {},
-            Token::CobaltGenerator => {},
-            Token::CuriumGenerator => {},
-            Token::RutheniumGenerator => {},
-            Token::PromethiumMicrochip => {},
-            Token::CobaltMicrochip => {},
-            Token::CuriumMicrochip => {},
-            Token::RutheniumMicrochip => {},
+        let mut floor_id = None;
+        let mut floor_inventory = Vec::new();
+        for token in tokens {
+            match token {
+                Entity::Floor(n) => floor_id = Some(n),
+                e @ _ => floor_inventory.push(e),
+            }
+        }
+        if let Some(f) = floor_id {
+            state.insert(f, floor_inventory);
+        }
+    }
+    println!("{:?}", state);
+
+    if let Some(top_floor) = state.iter().map(|s| {
+        let (f, _): (&usize, &Vec<Entity>) = s;
+        *f
+    }).max() {
+        let elevator = Elevator {
+            a: None,
+            b: None,
+            current_floor: 1,
+            top_floor: top_floor,
+        };
+
+        loop {
+            if true {
+                break;
+            }
         }
     }
     42
 }
 
 #[derive(Debug, Clone)]
-enum Token {
+enum Entity<'e> {
     Floor(usize),
-    PromethiumGenerator,
-    CobaltGenerator,
-    CuriumGenerator,
-    RutheniumGenerator,
-    PromethiumMicrochip,
-    CobaltMicrochip,
-    CuriumMicrochip,
-    RutheniumMicrochip,
+    Generator(&'e str),
+    Microchip(&'e str),
+}
+
+struct Elevator {
+    a: Option<usize>,
+    b: Option<usize>,
+    current_floor: usize,
+    top_floor: usize,
+}
+
+impl Elevator {
+    fn inc_floor(&mut self) {
+        if self.current_floor < self.top_floor {
+            self.current_floor += 1;
+        }
+    }
+
+    fn dec_floor(&mut self) {
+        if self.current_floor > 0 {
+            self.current_floor -= 1;
+        }
+    }
 }
