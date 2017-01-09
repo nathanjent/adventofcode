d2e3686c090d57d9c2537a1903eb1489539ddb31
nathanjent <nathanjent@gmail.com>
2017-01-09 06:36:08 -0500

    Add simple_json crate.
    Update regex and md5 crate versions.

--- src/day12.rs
+++ src/day12.rs
@@ -1,17 +1,13 @@
+extern crate simple_json;
+
+use self::simple_json::Json;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::collections::HashMap;
 
 pub fn abacus_framework_1(file: &str) -> i64 {
-    process(file)
-}
-
-pub fn abacus_framework_2(file: &str) -> i64 {
-    process(file)
-}
-
-fn process(file: &str) -> i64 {
     let input = File::open(file).expect("File open fail.");
     let reader = BufReader::new(input);
 
@@ -19,11 +15,26 @@ fn process(file: &str) -> i64 {
         .filter_map(Result::ok);
     let mut sum = 0;
     for line in lines {
-            sum += line.split(|c| !(c as char).is_digit(10) && c != '-')
-                //.inspect(|n| println!("numbers: {:?}", n))
-                .filter_map(|word| word.parse::<i64>().ok())
-                .inspect(|n| println!("numbers: {:?}", n))
-                .sum();
+        sum += line.split(|c| !(c as char).is_digit(10) && c != '-')
+            //.inspect(|n| println!("numbers: {:?}", n))
+            .filter_map(|word| word.parse::<i64>().ok())
+            .inspect(|n| print!("{:?}, ", n))
+            .sum();
+    }
+    sum
+}
+
+pub fn abacus_framework_2(file: &str) -> i64 {
+    let input = File::open(file).expect("File open fail.");
+    let reader = BufReader::new(input);
+
+    let lines = reader.lines()
+        .filter_map(Result::ok);
+    let mut sum = 0;
+    for line in lines {
+        if let Ok(json) = Json::parse(&*line) {
+            println!("{:?}", json.to_string());
         }
+    }
     sum
 }
