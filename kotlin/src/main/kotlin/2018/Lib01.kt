cec1b62dd1a77aa5d0bd2cc7187452a1a9b02ad6
Nathan Jent <nathanjent@gmail.com>
2018-12-01 21:55:50 -0500

    Update with completed solution to day 1 2018.
    I started to setup the tests using JUnit Theories, which Kotlin
    complicates. I soon realized I needed to test for different results
    depending on the data set. The JUnit Parameterized runner was exactly
    what I needed.
    
    This was my first experience writing Kotlin. I like some of the
    additional features that Java is missing but find myself trying to
    write Java then correct the syntax errors. I like that Kotlin can reuse
    all the Java libraries but I'm not always sure if I should use a Java
    library or find out the Kotlin way for doing something.

--- kotlin/src/main/kotlin/2018/Lib01.kt
+++ kotlin/src/main/kotlin/2018/Lib01.kt
@@ -1,5 +1,38 @@
-package y2018;
+package aoc.kt.y2018;
 
-fun process(input: String): String {
-    return input;
+/** Part 1 */
+fun process1(input: String): String {
+    val output = input.lines()
+    .filter { !it.isEmpty() }
+    .map(Integer::parseInt)
+    .sum()
+
+    return output.toString()
+}
+
+/** Part 2 */
+fun process2(input: String): String {
+    val inputs = input.lines()
+        .filter { !it.isEmpty() }
+        .map(Integer::parseInt)
+        .toList()
+
+    val (firstLoop, result) = inputs
+        .fold(Pair(mutableListOf<Int>(), 0),
+        { (freqs, acc), next ->
+            freqs.add(acc)
+            Pair(freqs, acc + next)
+        })
+
+    var sum = result
+    for (_i in 1..999) {
+        for (num in inputs) {
+            if (firstLoop.contains(sum)) {
+                return sum.toString()
+            }
+            sum += num
+        }
+    }
+
+    return "It's over 9000!"
 }
--- kotlin/src/test/kotlin/2018/Test01.kt
+++ kotlin/src/test/kotlin/2018/Test01.kt
@@ -1,11 +1,98 @@
-package y2018;
+package aoc.kt.y2018;
 
 import kotlin.test.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+import org.junit.experimental.theories.DataPoints
+import java.io.File
 
-class Test01 {
+/**
+ * Day 1 tests. Parameterized with expected results
+ * for both methods since the data is the same.
+ */
+@RunWith(Parameterized::class)
+class Test01(
+        val input: String,
+        val expectedProcess1: String,
+        val expectedProcess2: String) {
+    companion object {
+        @JvmStatic
+        @Parameters(name = "{index}: process1({0})={1}, process2({0})={2}")
+        fun inputs(): Collection<Array<String>> {
+            return listOf(
+                    arrayOf(
+                            """
+                            |+1
+                            |-2
+                            |+3
+                            |+1
+                            """.trimMargin(), "3", "2"),
+                    arrayOf(
+                            """
+                            |+1
+                            |+1
+                            |+1
+                            """.trimMargin(),"3", "It's over 9000!"),
+                    arrayOf(
+                            """
+                            |+1
+                            |+1
+                            |-2
+                            """.trimMargin(), "0", "0"),
+                    arrayOf(
+                            """
+                            |-1
+                            |-2
+                            |-3
+                            """.trimMargin() ,"-6", "It's over 9000!"),
+                    arrayOf(
+                            """
+                            |+1
+                            |-1
+                            """.trimMargin() ,"0", "0"),
+                    arrayOf(
+                            """
+                            |+3
+                            |+3
+                            |+4
+                            |-2
+                            |-4
+                            """.trimMargin() ,"4", "10"),
+                    arrayOf(
+                            """
+                            |-6
+                            |+3
+                            |+8
+                            |+5
+                            |-6
+                            """.trimMargin(), "4", "5"),
+                    arrayOf(
+                            """
+                            |+7
+                            |+7
+                            |-2
+                            |-7
+                            |-4
+                            """.trimMargin(), "1", "14"),
+                    arrayOf(
+                            File("../input/2018/day01.txt")
+                            .readText(),
+                            "466", "750")
+            )
+        }
+    }
+
+    /** Part 1 */
     @Test
-    fun testA() {
-        assertEquals("", process(""));
+    public fun processTest() {
+        assertEquals(expectedProcess1, process1(input))
+    }
+
+    /** Part 2 */
+    @Test
+    public fun process2Test() {
+        assertEquals(expectedProcess2, process2(input))
     }
 }
