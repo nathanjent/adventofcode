5f9fcf0ddf9fa7c920c972402bff8a4af1296f85
Nathan Jent <nathanjent@gmail.com>
2018-12-06 06:18:49 -0500

    Add initial code for day 5 2018.

---
+++ kotlin/src/main/kotlin/2018/Lib05.kt
@@ -0,0 +1,30 @@
+package aoc.kt.y2018;
+
+/**
+ * Day 5.
+ */
+
+/** Part 1 */
+fun processPolymer1(input: String): String {
+    val output = input.toCharArray()
+    .forEachIndexed { i, c ->
+        if (i == 0) {
+            continue
+        }
+        var reacting = true
+        var range = 0..0
+        while (reacting) {
+            var offset = 0
+            if (reactionOccurs(c, input.get(-1))) {
+            } else {
+                reacting = false
+            }
+        }
+    }
+    return output.toString()
+}
+
+/** Part 2 */
+fun processPolymer2(input: String): String {
+    return "42"
+}
---
+++ kotlin/src/test/kotlin/2018/Test05.kt
@@ -0,0 +1,47 @@
+package aoc.kt.y2018;
+
+import kotlin.test.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+import org.junit.experimental.theories.DataPoints
+import java.io.File
+
+/**
+ * Day 3 tests. Parameterized with expected results
+ * for both methods since the data is the same.
+ */
+@RunWith(Parameterized::class)
+class Test03(
+        val input: String,
+        val expectedProcess1: String,
+        val expectedProcess2: String) {
+    companion object {
+        @JvmStatic
+        @Parameters(name = "{index}: processPolymer1({0})={1}, processPolymer2({0})={2}")
+        fun inputs(): Collection<Array<String>> {
+            return listOf(
+                    arrayOf(
+                            """
+                            |dabAcCaCBAcCcaDA
+                            """.trimMargin(), "10", "42")
+                    //        ,
+                    //arrayOf(File("../input/2018/day05.txt")
+                    //    .readText(), "42", "42")
+            )
+        }
+    }
+
+    /** Part 1 */
+    @Test
+    public fun processPolymerTest() {
+        assertEquals(expectedProcess1, processPolymer1(input))
+    }
+
+    /** Part 2 */
+    @Test
+    public fun processPolymer2Test() {
+        assertEquals(expectedProcess2, processPolymer2(input))
+    }
+}
