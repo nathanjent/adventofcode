812890f5893867cd178ef14fdc628fcd5e1e7a78
Nathan Jent <nathanjent@gmail.com>
2018-12-12 21:26:24 -0500

    Add initial code for day 6 2018

---
+++ kotlin/src/main/kotlin/2018/Lib06.kt
@@ -0,0 +1,66 @@
+package aoc.kt.y2018;
+
+import kotlin.math.absoluteValue
+
+/**
+ * Day 6.
+ */
+
+/** Part 1 */
+fun processAreas1(input: String): String {
+    val coordinateMap = input.lines()
+        .mapIndexed { i, line ->
+            val (xStr, yStr) = line.split(", ")
+            Pair(Point(xStr.toInt(), yStr.toInt()), i)
+        }
+        .toMap()
+
+    var width = -1
+    var height = -1
+    coordinateMap.keys.forEach {
+        if (it.x > width) width = it.x
+        if (it.y > height) height = it.y
+    }
+
+    val areaMap = mutableMapOf<Point, Point?>()
+    for (x in 0..width) {
+        for (y in 0..height) {
+            var maxDistance = -1
+            var maxCoordinatePoint: Point? = null
+            var pointHadEqual: Point? = null
+            val mapPoint = Point(x, y)
+            for ((coordinatePoint, _) in coordinateMap) {
+                val distance = manhattanDistance(coordinatePoint, mapPoint)
+                if (distance == maxDistance) {
+                    pointHadEqual = coordinatePoint
+                }
+                maxDistance = if (distance > maxDistance) {
+                    maxCoordinatePoint = coordinatePoint
+                    distance
+                } else {
+                    maxDistance
+                }
+            }
+            if (pointHadEqual == maxCoordinatePoint) {
+                maxCoordinatePoint = null
+            }
+            areaMap.put(mapPoint, maxCoordinatePoint)
+        }
+    }
+
+    return areaMap
+        .toString()
+}
+
+/** Part 2 */
+fun processAreas2(input: String): String {
+    return "42"
+}
+
+fun manhattanDistance(p1: Int, p2: Int, q1: Int, q2: Int): Int {
+    return (p1 - q1).absoluteValue + (p2 - q2).absoluteValue
+}
+
+fun manhattanDistance(p1: Point, p2: Point): Int {
+    return manhattanDistance(p1.x, p1.y, p2.x, p2.y)
+}
---
+++ kotlin/src/test/kotlin/2018/Test06.kt
@@ -0,0 +1,51 @@
+package aoc.kt.y2018;
+
+import kotlin.test.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+import org.junit.experimental.theories.DataPoints
+import java.io.File
+
+/**
+ * Day 6 tests. Parameterized with expected results
+ * for both methods since the data is the same.
+ */
+@RunWith(Parameterized::class)
+class Test06(
+        val input: String,
+        val expectedProcess1: String,
+        val expectedProcess2: String) {
+    companion object {
+        @JvmStatic
+        @Parameters(name = "{index}: process1({0})={1}, process2({0})={2}")
+        fun inputs(): Collection<Array<String>> {
+            return listOf(
+                    arrayOf("""
+                        |1, 1
+                        |1, 6
+                        |8, 3
+                        |3, 4
+                        |5, 5
+                        |8, 9
+                        """.trimMargin(), "17", "42")
+                    //,
+                    //arrayOf(File("../input/2018/day06.txt")
+                    //    .readText(), "42", "42")
+            )
+        }
+    }
+
+    /** Part 1 */
+    @Test
+    public fun processPolymerTest() {
+        assertEquals(expectedProcess1, processAreas1(input))
+    }
+
+    /** Part 2 */
+    @Test
+    public fun processPolymer2Test() {
+        assertEquals(expectedProcess2, processAreas2(input))
+    }
+}
