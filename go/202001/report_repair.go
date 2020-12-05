28561c9a16370764e39da7216af8f8e20e4a610e
Nathan Jent <nathanjent@gmail.com>
2020-12-05 08:30:50 -0500

    Add solution for day 1 2020 part 2

--- go/202001/report_repair.go
+++ go/202001/report_repair.go
@@ -7,18 +7,9 @@ import (
 )
 
 func FindTwoWithSum2020(input *string) (int64, error) {
-	var inputs = strings.Split(*input, "\n")
-	var numbers = make([]int64, len(inputs))
-	for i, line := range inputs {
-		line = strings.Trim(line, "\t ")
-		if line == "" {
-			continue
-		}
-		var number, err = strconv.ParseInt(line, 10, 64)
-		if err != nil {
-			return 0, err
-		}
-		numbers[i] = number
+	var numbers, err = ParseNumbers(input)
+	if err != nil {
+		return 0, err
 	}
 	for _, i := range numbers {
 		for _, j := range numbers {
@@ -28,4 +19,38 @@ func FindTwoWithSum2020(input *string) (int64, error) {
 		}
 	}
 	return 0, errors.New("not found")
+}
+
+func FindThreeWithSum2020(input *string) (int64, error) {
+	var numbers, err = ParseNumbers(input)
+	if err != nil {
+		return 0, err
+	}
+	for _, i := range numbers {
+		for _, j := range numbers {
+			for _, k := range numbers {
+				if i+j+k == 2020 {
+					return i * j * k, nil
+				}
+			}
+		}
+	}
+	return 0, errors.New("not found")
+}
+
+func ParseNumbers(input *string) ([]int64, error) {
+	var inputs = strings.Split(*input, "\n")
+	var numbers = make([]int64, len(inputs))
+	for i, line := range inputs {
+		line = strings.Trim(line, "\t ")
+		if line == "" {
+			continue
+		}
+		var number, err = strconv.ParseInt(line, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		numbers[i] = number
+	}
+	return numbers, nil
 }
--- go/202001/report_repair_test.go
+++ go/202001/report_repair_test.go
@@ -25,9 +25,36 @@ func TestFindTwoWithSum2020(t *testing.T) {
 		t.Fatalf("Failed to read \"input.txt\". %v", err)
 	}
 	input := string(data)
-	var want = int64(514579)
+	var want = int64(1014624)
 	result, err := FindTwoWithSum2020(&input)
 	if want != result || err != nil {
 		t.Fatalf(`FindTwoWithSum2020() => %v, %v, want match for %v, nil`, result, err, want)
+	}
+}
+
+func TestFindThreeWithSum2020Basic(t *testing.T) {
+	input := `1721
+979
+366
+299
+675
+1456`
+	var want = int64(241861950)
+	result, err := FindThreeWithSum2020(&input)
+	if want != result || err != nil {
+		t.Fatalf(`FindThreeWithSum2020() = %q, %v, want match for %v, nil`, input, err, want)
+	}
+}
+
+func TestFindThreeWithSum2020(t *testing.T) {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		t.Fatalf("Failed to read \"input.txt\". %v", err)
+	}
+	input := string(data)
+	var want = int64(80072256)
+	result, err := FindThreeWithSum2020(&input)
+	if want != result || err != nil {
+		t.Fatalf(`FindThreeWithSum2020() => %v, %v, want match for %v, nil`, result, err, want)
 	}
 }
