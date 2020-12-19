b1846103ce5ffd78601149abf271b3bb022e79a5
Nathan Jent <nathanjent@gmail.com>
2020-12-19 09:02:38 -0500

    Add solution for day 8 2020 part 2

--- go/202008/code.go
+++ go/202008/code.go
@@ -1,54 +1,103 @@
 package handheld_halting
 
 import (
-	"fmt"
+	"errors"
+	//"fmt"
 	"strconv"
 	"strings"
 )
 
-func AnalyzeInstructions(input *string) (int, error) {
+func CheckProgramForInfiniteLoop(input *string) (int, error) {
 	program, err := ParseInstructions(input)
 	if err != nil {
 		return 0, err
 	}
 
+	accumulator, err := RunProgram(program)
+	if !errors.Is(err, InfiniteLoopError) {
+		return 0, err
+	}
+
+	return accumulator, nil
+}
+
+func AutoCorrectingProgramExec(input *string) (int, error) {
+	program, err := ParseInstructions(input)
+	if err != nil {
+		return 0, err
+	}
+
+	accumulator, err := 0, nil
+	for cursor := 0; cursor < len(program); cursor++ {
+		// Attempt to correct current instruction
+		program[cursor].Operation = FlipOperation(program[cursor].Operation)
+		//fmt.Printf("%v\n", program)
+		accumulator, err = RunProgram(program)
+		if err == nil || !errors.Is(err, InfiniteLoopError) {
+			// Some other error occurred
+			break
+		}
+
+		// Revert the program modification
+		program[cursor].Operation = FlipOperation(program[cursor].Operation)
+	}
+
+	return accumulator, err
+}
+
+// Returns a NOP for a JMP or a JMP for a NOP instruction
+func FlipOperation(operation string) string {
+	switch operation {
+	case "nop":
+		return "jmp"
+	case "jmp":
+		return "nop"
+	default:
+		return operation
+	}
+}
+
+func RunProgram(program Program) (int, error) {
+	//fmt.Printf("\n%v\n", program)
 	accumulator := 0
 	visited := make([]bool, len(program))
 	counter := 0
-	for ; counter < len(program); {
-		fmt.Printf("%v\n", program[counter])
+	for counter < len(program) {
+		//fmt.Printf("%v\n", program[counter])
 		// stop program if instruction run a second time
 		if visited[counter] {
-			break;
+			return accumulator, InfiniteLoopError
 		}
 		visited[counter] = true
 		instruction := program[counter]
 		switch instruction.Operation {
 		case "acc":
-			fmt.Printf("acc %v => ", accumulator)
-			accumulator += instruction.value
-			fmt.Printf("%v\n", accumulator)
-			fmt.Printf("pc %v => ", counter)
+			//fmt.Printf("acc %v => ", accumulator)
+			accumulator += instruction.Value
+			//fmt.Printf("%v\n", accumulator)
+			//fmt.Printf("pc %v => ", counter)
 			counter++
-			fmt.Printf("%v\n", counter)
+			//fmt.Printf("%v\n", counter)
 		case "jmp":
-			fmt.Printf("jmp %v => ", counter)
-			counter += instruction.value
-			fmt.Printf("%v\n", counter)
+			//fmt.Printf("jmp %v => ", counter)
+			counter += instruction.Value
+			//fmt.Printf("%v\n", counter)
 		default:
 			// nop
-			fmt.Printf("pc %v => ", counter)
+			//fmt.Printf("pc %v => ", counter)
 			counter++
-			fmt.Printf("%v\n", counter)
+			//fmt.Printf("%v\n", counter)
 		}
 	}
 
 	return accumulator, nil
 }
 
+var InfiniteLoopError = errors.New("Infinite loop detected.")
+
 type Instruction struct {
 	Operation string
-	value int
+	Value     int
 }
 
 type Program = []Instruction
--- go/202008/code_test.go
+++ go/202008/code_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestProgramAnalysisBasic(t *testing.T) {
+func TestCheckProgramBasic(t *testing.T) {
 	input := `nop +0
 acc +1
 jmp +4
@@ -16,20 +16,50 @@ acc +1
 jmp -4
 acc +6`
 	var want = 5
-	result, err := AnalyzeInstructions(&input)
+	result, err := CheckProgramForInfiniteLoop(&input)
 	if want != result || err != nil {
 		t.Fatalf(`Error: "%v", want %v got %v`, err, want, result)
 	}
 }
 
-func TestProgramAnalysis(t *testing.T) {
+func TestCheckProgram(t *testing.T) {
 	data, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		t.Fatalf("Failed to read \"input.txt\". %v", err)
 	}
 	input := string(data)
 	var want = 1521
-	result, err := AnalyzeInstructions(&input)
+	result, err := CheckProgramForInfiniteLoop(&input)
+	if want != result || err != nil {
+		t.Fatalf(`Error: "%v", want %v got %v`, err, want, result)
+	}
+}
+
+func TestAutoCorrectingProgramExecBasic(t *testing.T) {
+	input := `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
+	var want = 8
+	result, err := AutoCorrectingProgramExec(&input)
+	if want != result || err != nil {
+		t.Fatalf(`Error: "%v", want %v got %v`, err, want, result)
+	}
+}
+
+func TestAutoCorrectingProgramExec(t *testing.T) {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		t.Fatalf("Failed to read \"input.txt\". %v", err)
+	}
+	input := string(data)
+	var want = 1016
+	result, err := AutoCorrectingProgramExec(&input)
 	if want != result || err != nil {
 		t.Fatalf(`Error: "%v", want %v got %v`, err, want, result)
 	}
